269683e4202743f9403e8fbe3573e6e3a560b93a
yuvikakathaith <yuvikakathaith3103@gmail.com>
2025-09-25 09:35:09 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images:{
-    domains: ['images.unsplash.com','randomuser.me'],
+    domains: ['randomuser.me', 'images.unsplash.com'],
   }
 };
 
